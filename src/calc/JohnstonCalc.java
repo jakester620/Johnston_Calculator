8692109ffd04a1491f795d5e10bbf278564db939
Jake <Jake@HeartEmoji>
2018-10-31 02:48:43 -0700

    Rough code/prior to testing and comments

---
+++ src/calc/JohnstonCalc.java
@@ -0,0 +1,15 @@
+package calc;
+
+/**
+ *
+ * @author Jake
+ */
+public class JohnstonCalc
+{
+    //the main method creates an object of type calculator
+
+    public static void main(String[] args)
+    {
+        Calc run = new Calc();
+    }
+}
